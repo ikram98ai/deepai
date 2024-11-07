5e6ed4a4b2eca071e62737d7e30cfc7950d68917
ikram98ai <ikram98ai@gmail.com>
2024-11-07 22:41:38 +0500

    implementing basic deeplearning learner framework

---
+++ baseai.py
@@ -0,0 +1,440 @@
+from __future__ import annotations
+from pathlib import Path
+from operator import itemgetter
+from itertools import zip_longest
+from collections.abc import Mapping
+from operator import attrgetter
+from functools import partial
+from copy import copy
+import pickle,gzip,os,time,shutil, random, math
+
+import numpy as np, matplotlib as mpl, matplotlib.pyplot as plt
+
+import torch
+import torch.nn.functional as F
+from torch import nn, tensor
+from torch.utils.data import default_collate
+from torch import optim
+from torch.optim.lr_scheduler import ExponentialLR
+from torch.utils.data import DataLoader, SequentialSampler, RandomSampler, BatchSampler
+from torcheval.metrics import MulticlassAccuracy,Mean
+
+import fastcore.all as fc
+from fastprogress import progress_bar,master_bar
+
+
+__all__ = ['accuracy', 'report', 'Dataset', 'fit', 'get_dls', 'inplace', 'collate_dict', 'show_image', 'subplots', 'get_grid', 'show_images', 'DataLoaders',
+           'def_device', 'conv', 'to_device', 'collate_device', 'CancelFitException', 'CancelBatchException', 'CancelEpochException', 'Callback', 'run_cbs',
+           'SingleBatchCB', 'to_cpu', 'MetricsCB', 'DeviceCB', 'TrainCB', 'ProgressCB', 'with_cbs', 'Learner', 'TrainLearner', 'MomentumLearner',
+           'LRFinderCB', 'lr_find', 'set_seed', 'Hook', 'Hooks', 'HooksCallback', 'append_stats', 'get_hist', 'get_min', 'ActivationStats'
+           ]
+
+def accuracy(out, yb): return (out.argmax(dim=1)==yb).float().mean()
+
+def report(loss, preds, yb): print(f'{loss:.2f}, {accuracy(preds, yb):.2f}')
+
+class Dataset():
+    def __init__(self, x, y): self.x,self.y = x,y
+    def __len__(self): return len(self.x)
+    def __getitem__(self, i): return self.x[i],self.y[i]
+
+
+def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
+    for epoch in range(epochs):
+        model.train()
+        for xb,yb in train_dl:
+            loss = loss_func(model(xb), yb)
+            loss.backward()
+            opt.step()
+            opt.zero_grad()
+
+        model.eval()
+        with torch.no_grad():
+            tot_loss,tot_acc,count = 0.,0.,0
+            for xb,yb in valid_dl:
+                pred = model(xb)
+                n = len(xb)
+                count += n
+                tot_loss += loss_func(pred,yb).item()*n
+                tot_acc  += accuracy (pred,yb).item()*n
+        print(epoch, tot_loss/count, tot_acc/count)
+    return tot_loss/count, tot_acc/count
+
+def get_dls(train_ds, valid_ds, bs, **kwargs):
+    return (DataLoader(train_ds, batch_size=bs, shuffle=True, **kwargs),
+            DataLoader(valid_ds, batch_size=bs*2, **kwargs))
+
+
+def inplace(f):
+    def _f(b):
+        f(b)
+        return b
+    return _f
+
+def collate_dict(ds):
+    get = itemgetter(*ds.features)
+    
+    def _f(b): return get(default_collate(b))
+    return _f
+
+class DataLoaders:
+    def __init__(self, *dls): self.train,self.valid = dls[:2]
+
+    @classmethod
+    def from_dd(cls, dd, batch_size, as_tuple=True, **kwargs):
+        f = collate_dict(dd['train'])
+        return cls(*get_dls(*dd.values(), bs=batch_size, collate_fn=f, **kwargs))
+
+@fc.delegates(plt.Axes.imshow)
+def show_image(im, ax=None, figsize=None, title=None, noframe=True, **kwargs):
+    "Show a PIL or PyTorch image on `ax`."
+    if fc.hasattrs(im, ('cpu','permute','detach')):
+        im = im.detach().cpu()
+        if len(im.shape)==3 and im.shape[0]<5: im=im.permute(1,2,0)
+    elif not isinstance(im,np.ndarray): im=np.array(im)
+    if im.shape[-1]==1: im=im[...,0]
+    if ax is None: _,ax = plt.subplots(figsize=figsize)
+    ax.imshow(im, **kwargs)
+    if title is not None: ax.set_title(title)
+    ax.set_xticks([]) 
+    ax.set_yticks([]) 
+    if noframe: ax.axis('off')
+    return ax
+
+@fc.delegates(plt.subplots, keep=True)
+def subplots(
+    nrows:int=1, # Number of rows in returned axes grid
+    ncols:int=1, # Number of columns in returned axes grid
+    figsize:tuple=None, # Width, height in inches of the returned figure
+    imsize:int=3, # Size (in inches) of images that will be displayed in the returned figure
+    suptitle:str=None, # Title to be set to returned figure
+    **kwargs
+): # fig and axs
+    "A figure and set of subplots to display images of `imsize` inches"
+    if figsize is None: figsize=(ncols*imsize, nrows*imsize)
+    fig,ax = plt.subplots(nrows, ncols, figsize=figsize, **kwargs)
+    if suptitle is not None: fig.suptitle(suptitle)
+    if nrows*ncols==1: ax = np.array([ax])
+    return fig,ax
+
+@fc.delegates(subplots)
+def get_grid(
+    n:int, # Number of axes
+    nrows:int=None, # Number of rows, defaulting to `int(math.sqrt(n))`
+    ncols:int=None, # Number of columns, defaulting to `ceil(n/rows)`
+    title:str=None, # If passed, title set to the figure
+    weight:str='bold', # Title font weight
+    size:int=14, # Title font size
+    **kwargs,
+): # fig and axs
+    "Return a grid of `n` axes, `rows` by `cols`"
+    if nrows: ncols = ncols or int(np.floor(n/nrows))
+    elif ncols: nrows = nrows or int(np.ceil(n/ncols))
+    else:
+        nrows = int(math.sqrt(n))
+        ncols = int(np.floor(n/nrows))
+    fig,axs = subplots(nrows, ncols, **kwargs)
+    for i in range(n, nrows*ncols): axs.flat[i].set_axis_off()
+    if title is not None: fig.suptitle(title, weight=weight, size=size)
+    return fig,axs
+
+@fc.delegates(subplots)
+def show_images(ims:list, # Images to show
+                nrows:int|None=None, # Number of rows in grid
+                ncols:int|None=None, # Number of columns in grid (auto-calculated if None)
+                titles:list|None=None, # Optional list of titles for each image
+                **kwargs):
+    "Show all images `ims` as subplots with `rows` using `titles`"
+    axs = get_grid(len(ims), nrows, ncols, **kwargs)[1].flat
+    for im,t,ax in zip_longest(ims, titles or [], axs): show_image(im, ax=ax, title=t)
+
+
+
+def conv(ni, nf, ks=3, stride=2, act=True):
+    res = nn.Conv2d(ni, nf, stride=stride, kernel_size=ks, padding=ks//2)
+    if act: res = nn.Sequential(res, nn.ReLU())
+    return res
+
+def_device = 'mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'
+
+def to_device(x, device=def_device):
+    if isinstance(x, torch.Tensor): return x.to(device)
+    if isinstance(x, Mapping): return {k:v.to(device) for k,v in x.items()}
+    return type(x)(to_device(o, device) for o in x)
+
+def collate_device(b): return to_device(default_collate(b))
+
+
+class CancelFitException(Exception): pass
+class CancelBatchException(Exception): pass
+class CancelEpochException(Exception): pass
+
+class Callback(): order = 0
+
+def run_cbs(cbs, method_nm, learn=None):
+    for cb in sorted(cbs, key=attrgetter('order')):
+        method = getattr(cb, method_nm, None)
+        if method is not None: method(learn)
+
+class SingleBatchCB(Callback):
+    order = 1
+    def after_batch(self, learn): raise CancelFitException()
+
+
+def to_cpu(x):
+    if isinstance(x, Mapping): return {k:to_cpu(v) for k,v in x.items()}
+    if isinstance(x, list): return [to_cpu(o) for o in x]
+    if isinstance(x, tuple): return tuple(to_cpu(list(x)))
+    res = x.detach().cpu()
+    return res.float() if res.dtype==torch.float16 else res
+
+class MetricsCB(Callback):
+    def __init__(self, *ms, **metrics):
+        for o in ms: metrics[type(o).__name__] = o
+        self.metrics = metrics
+        self.all_metrics = copy(metrics)
+        self.all_metrics['loss'] = self.loss = Mean()
+
+    def _log(self, d): print(d)
+    def before_fit(self, learn): learn.metrics = self
+    def before_epoch(self, learn): [o.reset() for o in self.all_metrics.values()]
+
+    def after_epoch(self, learn):
+        log = {k:f'{v.compute():.3f}' for k,v in self.all_metrics.items()}
+        log['epoch'] = learn.epoch
+        log['train'] = 'train' if learn.model.training else 'eval'
+        self._log(log)
+
+    def after_batch(self, learn):
+        x,y,*_ = to_cpu(learn.batch)
+        for m in self.metrics.values(): m.update(to_cpu(learn.preds), y)
+        self.loss.update(to_cpu(learn.loss), weight=len(x))
+
+class DeviceCB(Callback):
+    def __init__(self, device=def_device): fc.store_attr()
+    def before_fit(self, learn):
+        if hasattr(learn.model, 'to'): learn.model.to(self.device)
+    def before_batch(self, learn): learn.batch = to_device(learn.batch, device=self.device)
+
+class TrainCB(Callback):
+    def __init__(self, n_inp=1): self.n_inp = n_inp
+    def predict(self, learn): learn.preds = learn.model(*learn.batch[:self.n_inp])
+    def get_loss(self, learn): learn.loss = learn.loss_func(learn.preds, *learn.batch[self.n_inp:])
+    def backward(self, learn): learn.loss.backward()
+    def step(self, learn): learn.opt.step()
+    def zero_grad(self, learn): learn.opt.zero_grad()
+
+class ProgressCB(Callback):
+    order = MetricsCB.order+1
+    def __init__(self, plot=False): self.plot = plot
+    def before_fit(self, learn):
+        learn.epochs = self.mbar = master_bar(learn.epochs)
+        self.first = True
+        if hasattr(learn, 'metrics'): learn.metrics._log = self._log
+        self.losses = []
+        self.val_losses = []
+
+    def _log(self, d):
+        if self.first:
+            self.mbar.write(list(d), table=True)
+            self.first = False
+        self.mbar.write(list(d.values()), table=True)
+
+    def before_epoch(self, learn): learn.dl = progress_bar(learn.dl, leave=False, parent=self.mbar)
+    def after_batch(self, learn):
+        learn.dl.comment = f'{learn.loss:.3f}'
+        if self.plot and hasattr(learn, 'metrics') and learn.training:
+            self.losses.append(learn.loss.item())
+            if self.val_losses: self.mbar.update_graph([[fc.L.range(self.losses), self.losses],[fc.L.range(learn.epoch).map(lambda x: (x+1)*len(learn.dls.train)), self.val_losses]])
+    
+    def after_epoch(self, learn): 
+        if not learn.training:
+            if self.plot and hasattr(learn, 'metrics'): 
+                self.val_losses.append(learn.metrics.all_metrics['loss'].compute())
+                self.mbar.update_graph([[fc.L.range(self.losses), self.losses],[fc.L.range(learn.epoch+1).map(lambda x: (x+1)*len(learn.dls.train)), self.val_losses]])
+
+class with_cbs:
+    def __init__(self, nm): self.nm = nm
+    def __call__(self, f):
+        def _f(o, *args, **kwargs):
+            try:
+                o.callback(f'before_{self.nm}')
+                f(o, *args, **kwargs)
+                o.callback(f'after_{self.nm}')
+            except globals()[f'Cancel{self.nm.title()}Exception']: pass
+            finally: o.callback(f'cleanup_{self.nm}')
+        return _f
+
+class Learner():
+    def __init__(self, model, dls=(0,), loss_func=F.mse_loss, lr=0.1, cbs=None, opt_func=optim.SGD):
+        cbs = fc.L(cbs)
+        fc.store_attr()
+
+    @with_cbs('batch')
+    def _one_batch(self):
+        self.predict()
+        self.callback('after_predict')
+        self.get_loss()
+        self.callback('after_loss')
+        if self.training:
+            self.backward()
+            self.callback('after_backward')
+            self.step()
+            self.callback('after_step')
+            self.zero_grad()
+
+    @with_cbs('epoch')
+    def _one_epoch(self):
+        for self.iter,self.batch in enumerate(self.dl): self._one_batch()
+
+    def one_epoch(self, training):
+        self.model.train(training)
+        self.dl = self.dls.train if training else self.dls.valid
+        self._one_epoch()
+
+    @with_cbs('fit')
+    def _fit(self, train, valid):
+        for self.epoch in self.epochs:
+            if train: self.one_epoch(True)
+            if valid: torch.no_grad()(self.one_epoch)(False)
+
+    def fit(self, n_epochs=1, train=True, valid=True, cbs=None, lr=None):
+        cbs = fc.L(cbs)
+        # `add_cb` and `rm_cb` were added in lesson 18
+        for cb in cbs: self.cbs.append(cb)
+        try:
+            self.n_epochs = n_epochs
+            self.epochs = range(n_epochs)
+            if lr is None: lr = self.lr
+            if self.opt_func: self.opt = self.opt_func(self.model.parameters(), lr)
+            self._fit(train, valid)
+        finally:
+            for cb in cbs: self.cbs.remove(cb)
+
+    def __getattr__(self, name):
+        if name in ('predict','get_loss','backward','step','zero_grad'): return partial(self.callback, name)
+        raise AttributeError(name)
+
+    def callback(self, method_nm): run_cbs(self.cbs, method_nm, self)
+    
+    @property
+    def training(self): return self.model.training
+
+class TrainLearner(Learner):
+    def predict(self): self.preds = self.model(self.batch[0])
+    def get_loss(self): self.loss = self.loss_func(self.preds, self.batch[1])
+    def backward(self): self.loss.backward()
+    def step(self): self.opt.step()
+    def zero_grad(self): self.opt.zero_grad()
+
+class MomentumLearner(TrainLearner):
+    def __init__(self, model, dls, loss_func, lr=None, cbs=None, opt_func=optim.SGD, mom=0.85):
+        self.mom = mom
+        super().__init__(model, dls, loss_func, lr, cbs, opt_func)
+
+    def zero_grad(self):
+        with torch.no_grad():
+            for p in self.model.parameters(): p.grad *= self.mom
+
+class LRFinderCB(Callback):
+    def __init__(self, gamma=1.3, max_mult=3): fc.store_attr()
+    
+    def before_fit(self, learn):
+        self.sched = ExponentialLR(learn.opt, self.gamma)
+        self.lrs,self.losses = [],[]
+        self.min = math.inf
+
+    def after_batch(self, learn):
+        if not learn.training: raise CancelEpochException()
+        self.lrs.append(learn.opt.param_groups[0]['lr'])
+        loss = to_cpu(learn.loss)
+        self.losses.append(loss)
+        if loss < self.min: self.min = loss
+        if math.isnan(loss) or (loss > self.min*self.max_mult):
+            raise CancelFitException()
+        self.sched.step()
+
+    def cleanup_fit(self, learn):
+        plt.plot(self.lrs, self.losses)
+        plt.xscale('log')
+
+@fc.patch
+def lr_find(self:Learner, gamma=1.3, max_mult=3, start_lr=1e-5, max_epochs=10):
+    self.fit(max_epochs, lr=start_lr, cbs=LRFinderCB(gamma=gamma, max_mult=max_mult))
+
+
+
+def set_seed(seed, deterministic=False):
+    torch.use_deterministic_algorithms(deterministic)
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+class Hook():
+    def __init__(self, m, f): self.hook = m.register_forward_hook(partial(f, self))
+    def remove(self): self.hook.remove()
+    def __del__(self): self.remove()
+
+class Hooks(list):
+    def __init__(self, ms, f): super().__init__([Hook(m, f) for m in ms])
+    def __enter__(self, *args): return self
+    def __exit__ (self, *args): self.remove()
+    def __del__(self): self.remove()
+    def __delitem__(self, i):
+        self[i].remove()
+        super().__delitem__(i)
+    def remove(self):
+        for h in self: h.remove()
+
+class HooksCallback(Callback):
+    def __init__(self, hookfunc, mod_filter=fc.noop, on_train=True, on_valid=False, mods=None):
+        fc.store_attr()
+        super().__init__()
+    
+    def before_fit(self, learn):
+        if self.mods: mods=self.mods
+        else: mods = fc.filter_ex(learn.model.modules(), self.mod_filter)
+        self.hooks = Hooks(mods, partial(self._hookfunc, learn))
+
+    def _hookfunc(self, learn, *args, **kwargs):
+        if (self.on_train and learn.training) or (self.on_valid and not learn.training): self.hookfunc(*args, **kwargs)
+
+    def after_fit(self, learn): self.hooks.remove()
+    def __iter__(self): return iter(self.hooks)
+    def __len__(self): return len(self.hooks)
+
+def append_stats(hook, mod, inp, outp):
+    if not hasattr(hook,'stats'): hook.stats = ([],[],[])
+    acts = to_cpu(outp)
+    hook.stats[0].append(acts.mean())
+    hook.stats[1].append(acts.std())
+    hook.stats[2].append(acts.abs().histc(40,0,10))
+
+# Thanks to @ste for initial version of histgram plotting code
+def get_hist(h): return torch.stack(h.stats[2]).t().float().log1p()
+
+def get_min(h):
+    h1 = torch.stack(h.stats[2]).t().float()
+    return h1[0]/h1.sum(0)
+
+class ActivationStats(HooksCallback):
+    def __init__(self, mod_filter=fc.noop): super().__init__(append_stats, mod_filter)
+
+    def color_dim(self, figsize=(11,5)):
+        fig,axes = get_grid(len(self), figsize=figsize)
+        for ax,h in zip(axes.flat, self):
+            show_image(get_hist(h), ax, origin='lower')
+
+    def dead_chart(self, figsize=(11,5)):
+        fig,axes = get_grid(len(self), figsize=figsize)
+        for ax,h in zip(axes.flatten(), self):
+            ax.plot(get_min(h))
+            ax.set_ylim(0,1)
+
+    def plot_stats(self, figsize=(10,4)):
+        fig,axs = plt.subplots(1,2, figsize=figsize)
+        for h in self:
+            for i in 0,1: axs[i].plot(h.stats[i])
+        axs[0].set_title('Means')
+        axs[1].set_title('Stdevs')
+        plt.legend(fc.L.range(self))
