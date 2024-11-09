1aabb09d6b4a4ec126274a18349a638e45522b16
ikram98ai <ikram98ai@gmail.com>
2024-11-09 13:26:21 +0500

    adding implementation of initializations

--- baseai.py
+++ baseai.py
@@ -1,23 +1,23 @@
 from __future__ import annotations
-from pathlib import Path
-from operator import itemgetter
+from operator import itemgetter, attrgetter
 from itertools import zip_longest
 from collections.abc import Mapping
-from operator import attrgetter
 from functools import partial
 from copy import copy
-import pickle,gzip,os,time,shutil, random, math
+import random, math, sys,gc,traceback
 
-import numpy as np, matplotlib as mpl, matplotlib.pyplot as plt
+import numpy as np, matplotlib.pyplot as plt
 
 import torch
 import torch.nn.functional as F
-from torch import nn, tensor
+import torchvision.transforms.functional as TF
+from torch import nn
 from torch.utils.data import default_collate
 from torch import optim
+from torch.nn import init
 from torch.optim.lr_scheduler import ExponentialLR
-from torch.utils.data import DataLoader, SequentialSampler, RandomSampler, BatchSampler
-from torcheval.metrics import MulticlassAccuracy,Mean
+from torch.utils.data import DataLoader
+from torcheval.metrics import Mean
 
 import fastcore.all as fc
 from fastprogress import progress_bar,master_bar
@@ -26,7 +26,8 @@ from fastprogress import progress_bar,master_bar
 __all__ = ['accuracy', 'report', 'Dataset', 'fit', 'get_dls', 'inplace', 'collate_dict', 'show_image', 'subplots', 'get_grid', 'show_images', 'DataLoaders',
            'def_device', 'conv', 'to_device', 'collate_device', 'CancelFitException', 'CancelBatchException', 'CancelEpochException', 'Callback', 'run_cbs',
            'SingleBatchCB', 'to_cpu', 'MetricsCB', 'DeviceCB', 'TrainCB', 'ProgressCB', 'with_cbs', 'Learner', 'TrainLearner', 'MomentumLearner',
-           'LRFinderCB', 'lr_find', 'set_seed', 'Hook', 'Hooks', 'HooksCallback', 'append_stats', 'get_hist', 'get_min', 'ActivationStats'
+           'LRFinderCB', 'lr_find', 'set_seed', 'Hook', 'Hooks', 'HooksCallback', 'append_stats', 'get_hist', 'get_min', 'ActivationStats', 'clean_ipython_hist', 
+           'clean_tb', 'clean_mem', 'BatchTransformCB', 'GeneralRelu', 'plot_func', 'init_weights', 'lsuv_init', 'conv', 'get_model'
            ]
 
 def accuracy(out, yb): return (out.argmax(dim=1)==yb).float().mean()
@@ -438,3 +439,86 @@ class ActivationStats(HooksCallback):
         axs[0].set_title('Means')
         axs[1].set_title('Stdevs')
         plt.legend(fc.L.range(self))
+
+
+def clean_ipython_hist():
+    # Code in this function mainly copied from IPython source
+    if not 'get_ipython' in globals(): return
+    ip = get_ipython()
+    user_ns = ip.user_ns
+    ip.displayhook.flush()
+    pc = ip.displayhook.prompt_count + 1
+    for n in range(1, pc): user_ns.pop('_i'+repr(n),None)
+    user_ns.update(dict(_i='',_ii='',_iii=''))
+    hm = ip.history_manager
+    hm.input_hist_parsed[:] = [''] * pc
+    hm.input_hist_raw[:] = [''] * pc
+    hm._i = hm._ii = hm._iii = hm._i00 =  ''
+
+def clean_tb():
+    # h/t Piotr Czapla
+    if hasattr(sys, 'last_traceback'):
+        traceback.clear_frames(sys.last_traceback)
+        delattr(sys, 'last_traceback')
+    if hasattr(sys, 'last_type'): delattr(sys, 'last_type')
+    if hasattr(sys, 'last_value'): delattr(sys, 'last_value')
+
+def clean_mem():
+    clean_tb()
+    clean_ipython_hist()
+    gc.collect()
+    torch.cuda.empty_cache()
+
+class BatchTransformCB(Callback):
+    def __init__(self, tfm, on_train=True, on_val=True): fc.store_attr()
+
+    def before_batch(self, learn):
+        if (self.on_train and learn.training) or (self.on_val and not learn.training):
+            learn.batch = self.tfm(learn.batch)
+
+class GeneralRelu(nn.Module):
+    def __init__(self, leak=None, sub=None, maxv=None):
+        super().__init__()
+        self.leak,self.sub,self.maxv = leak,sub,maxv
+
+    def forward(self, x): 
+        x = F.leaky_relu(x,self.leak) if self.leak is not None else F.relu(x)
+        if self.sub is not None: x -= self.sub
+        if self.maxv is not None: x.clamp_max_(self.maxv)
+        return x
+
+def plot_func(f, start=-5., end=5., steps=100):
+    x = torch.linspace(start, end, steps)
+    plt.plot(x, f(x))
+    plt.grid(True, which='both', ls='--')
+    plt.axhline(y=0, color='k', linewidth=0.7)
+    plt.axvline(x=0, color='k', linewidth=0.7)
+
+def init_weights(m, leaky=0.):
+    if isinstance(m, (nn.Conv1d,nn.Conv2d,nn.Conv3d)): init.kaiming_normal_(m.weight, a=leaky)
+
+def _lsuv_stats(hook, mod, inp, outp):
+    acts = to_cpu(outp)
+    hook.mean = acts.mean()
+    hook.std = acts.std()
+
+def lsuv_init(model, m, m_in, xb):
+    h = Hook(m, _lsuv_stats)
+    with torch.no_grad():
+        while model(xb) is not None and (abs(h.std-1)>1e-3 or abs(h.mean)>1e-3):
+            m_in.bias -= h.mean
+            m_in.weight.data /= h.std
+    h.remove()
+
+def conv(ni, nf, ks=3, stride=2, act=nn.ReLU, norm=None, bias=None):
+    if bias is None: bias = not isinstance(norm, (nn.BatchNorm1d,nn.BatchNorm2d,nn.BatchNorm3d))
+    layers = [nn.Conv2d(ni, nf, stride=stride, kernel_size=ks, padding=ks//2, bias=bias)]
+    if norm: layers.append(norm(nf))
+    if act: layers.append(act())
+    return nn.Sequential(*layers)
+
+def get_model(act=nn.ReLU, nfs=None, norm=None):
+    if nfs is None: nfs = [1,8,16,32,64]
+    layers = [conv(nfs[i], nfs[i+1], act=act, norm=norm) for i in range(len(nfs)-1)]
+    return nn.Sequential(*layers, conv(nfs[-1],10, act=None, norm=False, bias=True),
+                         nn.Flatten()).to(def_device)
