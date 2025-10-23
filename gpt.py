30169959708074e8211f2aaad9e0fa5e5398ae9a
ikram98ai <ikram98ai@gmail.com>
2025-10-23 15:47:03 +0500

    improving the gpt model

--- gpt.py
+++ gpt.py
@@ -24,6 +24,9 @@ block_size=8
 batch_size=32
 learning_rate=1e-3
 n_embd=32
+n_layer=3
+n_head=4
+dropout=0.2
 max_iters = 5000
 eval_iters= 300
 eval_interval= 300
@@ -52,33 +55,75 @@ def estimate_loss():
     model.train()
     return out
 
+class FeedForward(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4*n_embd),
+            nn.ReLU(),
+            nn.Linear(4*n_embd, n_embd),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
 class Head(nn.Module):
     def __init__(self, head_size):
         super().__init__()
         self.key = nn.Linear(n_embd, head_size, bias=False)
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
-
-        self.register_buffer("tril", torch.ones(block_size,block_size))
+        self.dropout= nn.Dropout(dropout)
+        self.register_buffer("tril", torch.tril(torch.ones(block_size,block_size)))
 
     def forward(self, x):
         B,T,C = x.shape
         k = self.key(x) # (B,T,C)
         q = self.query(x) # (B,T,C)
-        v = self.value(x) # (B,T,C)
 
         wei:Tensor = q @ k.transpose(-2,-1) * C**-0.5 # (B,T,C) @ (B,C,T) ->  (B,T,T)
-        wei = wei.masked_fill(self.tril[:T,:T]==0, float('inf')) # (B,T,T)
+        wei = wei.masked_fill(self.tril[:T,:T]==0, float('-inf')) # (B,T,T)
         wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
+        v = self.value(x) # (B,T,C)
         out = wei @ v # (B,T,T) @ (B,T,C)
         return out
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self,n_head, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(n_head)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout= nn.Dropout(dropout)
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+
+class Block(nn.Module):
+    def __init__(self, n_head):
+        super().__init__()
+        head_size = n_embd//n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd= FeedForward()
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
 
 class GPTModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.token_embedding = nn.Embedding(vocab_size,n_embd)
         self.position_embedding = nn.Embedding(block_size,n_embd)
-        self.sa_head = Head(n_embd)
+        self.blocks = nn.Sequential(*[Block(n_head) for _ in range(n_layer)] ) 
+        self.lnf= nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
 
@@ -87,7 +132,8 @@ class GPTModel(nn.Module):
         tok_emb = self.token_embedding(idx) # (B,T,C)
         pos_emb = self.position_embedding(torch.arange(T,device=device)) # (T,C)
         x = tok_emb + pos_emb
-        x = self.sa_head(x)
+        x = self.blocks(x)
+        x = self.lnf(x)
         logits = self.lm_head(x) # (B,T,vocab_size)
 
         if target is None:
@@ -113,6 +159,7 @@ class GPTModel(nn.Module):
 
 model = GPTModel()
 m = model.to(device)
+model.compile()
 
 optimizer = torch.optim.AdamW(model.parameters(),lr=learning_rate)
 
